e77c0dc16acbce3fa0b204c0c36173346d332cc3
GayathriNairMars <emp2mayalabs@gmail.com>
2026-05-27 16:16:37 +0530

    updated the username section

--- src/pages/admin/dashboard/superadmindashboard.jsx
+++ src/pages/admin/dashboard/superadmindashboard.jsx
@@ -119,16 +119,15 @@ export default function SuperAdminDashboard() {
             </div>
 
             <div className="user-area">
-              <button className="notification-btn-square">
-                <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="var(--accent)" strokeWidth="2">
-                  <rect x="4" y="4" width="16" height="16" rx="3" />
-                  <path d="M9 12h6" />
-                  <path d="M12 9v6" />
-                </svg>
-                <span className="dot-red"></span>
-              </button>
-
               <div className="topbar-profile">
+              	<button className="notification-btn-square">
+                	<svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="var(--accent)" strokeWidth="2">
+                  	<rect x="4" y="4" width="16" height="16" rx="3" />
+                  	<path d="M9 12h6" />
+                  	<path d="M12 9v6" />
+                	</svg>
+                	<span className="dot-red"></span>
+              	</button>
                 <div className="profile-details">
                   <span className="profile-name">{user?.full_name || "Arathy Sreekumar"}</span>
                   <span className="profile-role">{ROLE_LABELS[user?.role] || user?.role || "System Administrator"}</span>
