d10b709cd6c4d8f34b85bf238923c1be15753157
GayathriNairMars <emp2mayalabs@gmail.com>
2026-05-27 15:48:09 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
  
 function getCookie(name) {
-  const value = ; ${document.cookie};
-  const parts = value.split(; ${name}=);
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(";").shift();
   return null;
 }
